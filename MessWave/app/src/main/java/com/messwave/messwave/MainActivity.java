376baa655e5e86d0c1022432959a07b9a0f516d3
KatteKurach <kattekurach@gmail.com>
2016-09-12 23:34:27 +0300

    Can get messages

--- MessWave/app/src/main/java/com/messwave/messwave/MainActivity.java
+++ MessWave/app/src/main/java/com/messwave/messwave/MainActivity.java
@@ -1,9 +1,34 @@
 package com.messwave.messwave;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       // VKSdk.initialize(this);
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
@@ -26,7 +53,62 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_build_black_24dp);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_assignment_black_24dp);
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_local_post_office_black_24dp);
+
     }
+
+
+    private void getLongPollServer() {
+        try {
+            Map<String, Object> vkparams = new HashMap<>();
+            vkparams.put("count", 200);
+            vkparams.put("out", 0);
+
+            VKRequest obj = (VKRequest) VKApi.messages().get();
+            obj.addExtraParameters(new VKParameters(vkparams));
+            obj.executeWithListener(new VKRequest.VKRequestListener() {
+                @Override
+                public void onComplete(VKResponse response) {
+                    super.onComplete(response);
+                    JSONObject parser = null;
+                    try {
+                        parser = (response.json).getJSONObject("response");
+                        JSONArray array = parser.getJSONArray("items");
+
+                        for (int i = 0; i < array.length(); i++){
+                           // Log.d("KATRIN", )
+
+                            String chatname = array.getJSONObject(i).getString("title").toString();
+                            String body = array.getJSONObject(i).getString("body").toString();
+                            Log.d("KATRIN", chatname+": "+body);
+                        }
+                        //Log.d("KATRIN", array.getJSONObject(0).toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    //Log.d("KATRIN", String.valueOf(response.json));
+                }
+            });
+
+        } catch (Exception e) {
+            Log.d("KATRIN", String.valueOf(e));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (VKSdk.isLoggedIn()) {
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    getLongPollServer();
+                }
+            });
+            t.start();
+        }
+    }
+
 
     private void setupViewPager(ViewPager viewPager) {
 
--- MessWave/app/src/main/java/com/messwave/messwave/SettingsFragment.java
+++ MessWave/app/src/main/java/com/messwave/messwave/SettingsFragment.java
@@ -1,11 +1,15 @@
 package com.messwave.messwave;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.vk.sdk.VKSdk;
 
 
 public class SettingsFragment extends Fragment {
@@ -23,7 +27,22 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        view.findViewById(R.id.vk_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("KATRIN", "Checking if logged");
+                //VKSdk.initialize(getActivity());
+                if (!VKSdk.isLoggedIn()) {
+                    Log.d("KATRING", "NOT LOGGED");
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    Log.d("KATRIN", "Start activity");
+                    startActivity(intent);
+                }
+            }
+        });
+        return view;
     }
 
 }
